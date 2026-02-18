e78e77ef5a727ef22dfa3450394c8b09fb6995c6
Rudraksh Mishra <mrudraksh03@gmail.com>
2026-02-18 15:28:12 +0530

    main

--- Leetcode/src/Leetcode/Palindrome_Partitioning_131.java
+++ Leetcode/src/Leetcode/Palindrome_Partitioning_131.java
@@ -1,19 +1,42 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Palindrome_Partitioning_131 {
     public static void main(String[] args) {
         String ques = "nitin";
-        Partition(ques,"");
+        List<String> ll = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
+        Partition(ques,ll,ans);
+        System.out.println(ans);
     }
-    public static void Partition(String ques , String ans){
+    public static void Partition(String ques , List<String> ll,List<List<String>> ans){
         if(ques.length()==0){
-            System.out.println(ans);
+//            System.out.println(ans);
+            ans.add(new ArrayList<>(ll));
             return;
         }
         for(int cut =1 ; cut<=ques.length(); cut++){
                 String s = ques.substring(0,cut);
-                Partition(ques.substring(cut),ans+s+"|");
+                if(isPalindrom(s)) {
+                    ll.add(s);
+                    Partition(ques.substring(cut), ll,ans);
+                    ll.remove(ll.size()-1);
+                }
         }
+    }
+    public static boolean isPalindrom(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
 
---
+++ Recursion/src/Rat_Chases.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Rat_Chases {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[][] maze = new char[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                maze[i][j] = sc.next(;
+            }
+        }
+
+    }
+}
