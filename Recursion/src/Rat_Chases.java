06e85ca0663847772884b831b1a9830039e1c4ea
Rudraksh Mishra <mrudraksh03@gmail.com>
2026-02-18 15:43:21 +0530

    commit this change

--- Recursion/src/Rat_Chases.java
+++ Recursion/src/Rat_Chases.java
@@ -7,6 +7,7 @@ public class Rat_Chases {
         int m = sc.nextInt();
         char[][] maze = new char[n][m];
         for(int i=0;i<n;i++){
+            String s = sc.next();
             for(int j=0;j<m;j++){
                 maze[i][j] = sc.next(;
             }
