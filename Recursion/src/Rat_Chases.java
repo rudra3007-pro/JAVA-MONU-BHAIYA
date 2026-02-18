d595c56afb248b29951caffa6df316c5bb5a46a5
Rudraksh Mishra <mrudraksh03@gmail.com>
2026-02-18 16:25:24 +0530

    main

--- Recursion/src/Rat_Chases.java
+++ Recursion/src/Rat_Chases.java
@@ -1,17 +1,59 @@
 import java.util.Scanner;
 
 public class Rat_Chases {
+    static int val = 0 ;
+
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        char[][] maze = new char[n][m];
-        for(int i=0;i<n;i++){
+        char [][] maze = new char [n][m] ;
+        for(int i =0 ; i < maze.length; i++) {
             String s = sc.next();
-            for(int j=0;j<m;j++){
-                maze[i][j] = sc.next(;
+            for(int j = 0 ; j < s.length() ; j++) {
+                maze[i][j] = s.charAt(j);
             }
         }
+        int [][] ans = new int[n][m];
+        print(maze , 0 ,0, ans);
 
+    }
+
+    public static void print(char [] [] maze , int cr , int cc , int ans[][]) {
+        if (cr < 0 || cc < 0 || cr >= maze.length || cc >= maze[0].length || maze[cr][cc] == 'X') {
+
+            return ;
+        }
+
+        if(cr == maze.length -1  && cc== maze[0].length-1) {
+            display(ans);
+        }
+
+        maze[cr][cc] = 'X';
+        ans[cr][cc] =1 ;
+        int r[] = { -1 ,0,1,0};
+        int c[] = {0,-1 ,0,1};
+
+
+
+        for(int i = 0 ; i < c.length ; i++) {
+            print(maze , cr+r[i] , cc+c[i] ,ans);
+        }
+//		 print(maze , cr-1 , cc, ans);
+//		 print(maze , cr , cc-1,ans);
+//		 print(maze , cr+1 , cc,ans);
+//		 print(maze , cr , cc +1 ,ans);
+        maze[cr][cc] = 'O';
+        ans[cr][cc] = 0 ;
+    }
+
+    public static void display(int[][] ans) {
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
